1c6d0259baf41ff22881ed9e673c2cda3d15fd31
omgitsmikee <github@michaelmak.ca>
2016-08-25 19:45:25 -0400

    Initial Commit

---
+++ Selfigram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Selfigram
+//
+//  Created by Michael Mak on 2016-08-25.
+//  Copyright © 2016 Michael Mak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
