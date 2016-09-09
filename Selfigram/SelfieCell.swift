9a6f342ec814f70097630fa075f349760f473299
omgitsmikee <github@michaelmak.ca>
2016-09-08 20:53:30 -0400

    Week 3 Day 2
    Added SelfieCell class
    Added camera button
    Added image picker

---
+++ Selfigram/SelfieCell.swift
@@ -0,0 +1,29 @@
+//
+//  SelfieCell.swift
+//  Selfigram
+//
+//  Created by Michael Mak on 2016-09-08.
+//  Copyright © 2016 Michael Mak. All rights reserved.
+//
+
+import UIKit
+
+class SelfieCell: UITableViewCell {
+
+    @IBOutlet weak var selfieImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
